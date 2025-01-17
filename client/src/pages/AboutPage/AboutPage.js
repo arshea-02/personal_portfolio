e2f4700ac5656a5dbcad2fa81852763b337a944f
arshea-02 <arsheaatif04@gmail.com>
2025-01-17 17:30:39 +0500

    Update About Me

--- client/src/pages/AboutPage/AboutPage.js
+++ client/src/pages/AboutPage/AboutPage.js
@@ -11,12 +11,8 @@ const AboutPage = () => {
           </div>
           <div className="col-xl-6 col-lg-6 col-md-6 col-xs-12 about-content">
             <h1>About Me</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Voluptatem adipisci, quis vitae sapiente explicabo accusantium
-              quibusdam molestiae sequi quod minima harum laudantium, ex quo
-              facere assumenda in nihil? Incidunt, perferendis?
-            </p>
+            <p>As a motivated individual with a strong passion for web development and Python programming, I bring a proven ability to collaborate effectively within teams and communicate complex ideas with clarity. My experience includes working on diverse academic projects, internships and extracurricular initiatives, where I honed my problem-solving skills and developed a strong technical foundation. Beyond my academic and professional endeavors, I have also volunteered for various social improvement programs, where I engaged with individuals from diverse backgrounds and contributed to meaningful change.</p>
+            <p>Committed to continuous learning and professional growth, I am motivated to drive success and deliver value in the tech industry of Pakistan.</p>
           </div>
         </div>
       </div>
